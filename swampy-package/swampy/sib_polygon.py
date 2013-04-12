e80bef84d6a62f565b5fa085b0c16b0b40d14f1d
Brian Yee <yee.brianj@gmail.com>
2013-04-11 22:28:18 -0400

    new polygon circle and arc functions

--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -2,7 +2,7 @@
 
 # Name:
 
-
+from math import pi
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
@@ -10,11 +10,32 @@ bob = Turtle()
 
 
 # This is where you put code to move bob
+def polygon(t, l, n):
+    angle = 360./n
+    for sides in range(n):
+        drawSide(t, l, angle)
 
+def circle(t, r):
+    circumference = 2*pi*r
+    n = r / 2
+    l = circumference / n
+    polygon(t, l, n)
 
+def arc(t, r, theta):
+    circumference = 2*pi*r
+    n = r / 2
+    l = circumference / n
+    angle = 360. / n
+    for sides in range(int(theta/angle)):
+        drawSide(t, l, angle)
 
+def drawSide(t, l, angle):
+    fd(t, l)
+    rt(t, angle)
 
-
+#polygon(bob, 100, 10)
+#circle(bob, 100)
+#arc(bob, 100, 90)
 
 
 
