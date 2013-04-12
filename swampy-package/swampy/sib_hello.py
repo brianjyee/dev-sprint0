2e43f95f9c92c73739eec973845dd3254001d3f4
Brian Yee <yee.brianj@gmail.com>
2013-04-11 22:27:39 -0400

    draw hello

--- swampy-package/swampy/sib_hello.py
+++ swampy-package/swampy/sib_hello.py
@@ -1,6 +1,6 @@
 # Hello excercise from Week 0
 
-# Name:
+# Name: Brian Yee
 
 
 from TurtleWorld import * 		
@@ -10,9 +10,57 @@ bob = Turtle()
 
 
 # This is where you put code to move bob
+def drawHello(t):
+    lt(t, 90)
+    fd(t, 100)
+    bk(t, 50)
+    rt(t, 90)
+    fd(t, 50)
+    lt(t, 90)
+    fd(t, 50)
+    bk(t, 100)
+    pu(t)
+    rt(t, 90)
+    fd(t, 20)
+    pd(t)
+    fd(t, 50)
+    bk(t, 50)
+    lt(t, 90)
+    fd(t, 50)
+    rt(t, 90)
+    fd(t, 25)
+    bk(t, 25)
+    lt(t, 90)
+    fd(t, 50)
+    rt(t, 90)
+    fd(t, 50)
+    pu(t)
+    fd(t, 25)
+    pd(t)
+    rt(t, 90)
+    fd(t, 100)
+    lt(t, 90)
+    fd(t, 50)
+    pu(t)
+    fd(t, 25)
+    pd(t)
+    fd(t, 50)
+    bk(t, 50)
+    lt(t, 90)
+    fd(t, 100)
+    rt(t, 90)
+    pu(t)
+    fd(t, 75)
+    pd(t)
+    fd(t, 50)
+    rt(t, 90)
+    fd(t, 100)
+    rt(t, 90)
+    fd(t, 50)
+    rt(t, 90)
+    fd(t, 100)
 
-
-
+drawHello(bob)
 
 
 
