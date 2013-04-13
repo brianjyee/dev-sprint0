8d7722cf05f6421f95ab55ca6535ff48551d5a7b
Brian Yee <yee.brianj@gmail.com>
2013-04-12 22:46:57 -0400

    new flower functions

--- swampy-package/swampy/sib_flower.py
+++ swampy-package/swampy/sib_flower.py
@@ -1,8 +1,8 @@
 # Flower excercise (4.2) from Week 0
 
-# Name:
+# Name: Brian Yee
 
-
+from sib_polygon import arc
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
@@ -10,11 +10,20 @@ bob = Turtle()
 
 
 # This is where you put code to move bob
+def drawFlower(t, r, theta, petals):
+    for petal in range(petals):
+        drawPetal(t, r, theta)
+        lt(t, 360./petals)
 
-
-
-
-
+def drawPetal(t, r, theta):
+    for i in range(2):
+        arc(t, r, theta)
+        rt(t, 180-theta)
+    
+#drawPetal(bob, 100, 45)
+#drawFlower(bob, 60., 60., 7)
+#drawFlower(bob, 60., 80., 10)
+#drawFlower(bob, 150., 30., 20)
 
 
 
